6986fafe3b4e18cd62826cb1d0571e67a6e3f39c
Regis Hastings <25518602+regis-hastings@users.noreply.github.com>
2020-05-24 10:55:16 -0700

    allow user to enter path parameter

--- index.js
+++ index.js
@@ -2,16 +2,19 @@
 
 const fs = require('fs');
 const chalk = require('chalk');
+const path = require('path');
 
 const { lstat } = fs.promises;
 
-fs.readdir(process.cwd(), async (err, filenames) => {
+const targetDir = process.argv[2] || process.cwd();
+
+fs.readdir(targetDir, async (err, filenames) => {
   if (err) {
     console.log(err);
   }
 
   const statPromises = filenames.map(filename => {
-    return lstat(filename);
+    return lstat(path.join(targetDir, filename));
   });
 
   const allStats = await Promise.all(statPromises);
