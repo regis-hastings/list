67d8321f7a1bdeba08ba7411dd64dc6cc08d2de4
Regis Hastings <25518602+regis-hastings@users.noreply.github.com>
2020-05-23 20:09:16 -0700

    use Promis.all to handle lstat calls

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
-// const util = require('util'); - Method #2
-// const lstat = util.promisify(fs.lstat); - Method #2
 
-// Method #3
 const { lstat } = fs.promises;
 
 fs.readdir(process.cwd(), async (err, filenames) => {
@@ -12,26 +9,15 @@ fs.readdir(process.cwd(), async (err, filenames) => {
     console.log(err);
   }
 
-  for (let filename of filenames) {
-    try {
-      const stats = await lstat(filename);
+  const statPromises = filenames.map(filename => {
+    return lstat(filename);
+  });
 
-      console.log(filename, stats.isFile());
-    } catch (err) {
-      console.log(err);
-    }
+  const allStats = await Promise.all(statPromises);
+
+  for (let stats of allStats) {
+    const index = allStats.indexOf(stats);
+
+    console.log(filenames[index], stats.isFile());
   }
 });
-
-// Method #1
-// const lstat = filename => {
-//   return new Promise((resolve, reject) => {
-//     fs.lstat(filename, (err, stats) => {
-//       if (err) {
-//         reject(err);
-//       }
-
-//       resolve(stats);
-//     });
-//   });
-// };
